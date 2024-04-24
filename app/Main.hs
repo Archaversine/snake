322673f86a8fe33bf013064c7f2de9b5e916920f
Archaversine <adam.brohl.w@gmail.com>
2024-04-23 20:03:50 -0500

    Add to git

---
+++ app/Main.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Main where
+
+import Apecs
+
+import Control.Monad (when)
+
+import Data.Aeson
+import Data.List (foldl')
+
+import GHC.Generics (Generic)
+
+import Raylib.Core
+import Raylib.Core.Shapes
+import Raylib.Core.Text
+
+import Raylib.Util
+import Raylib.Util.Colors
+import Raylib.Util.Math
+
+import Raylib.Types
+
+import System.Environment
+
+windowDims :: Num a => (a, a)
+windowDims = (800, 600)
+
+newtype Position    = Position Vector2
+newtype Velocity    = Velocity Vector2
+newtype Size        = Size Float
+newtype Mass        = Mass Float
+newtype EntityID    = EntityID Entity
+newtype CircleColor = CircleColor Color
+
+data Immovable = Immovable
+
+instance FromJSON Position where 
+    parseJSON = withObject "position" $ \v -> Position 
+        <$> (Vector2 <$> v .: "x" <*> v .: "y")
+
+instance FromJSON Velocity where 
+    parseJSON = withObject "velocity" $ \v -> Velocity 
+        <$> (Vector2 <$> v .: "x" <*> v .: "y")
+
+instance FromJSON CircleColor where 
+    parseJSON = withObject "color" $ \v -> CircleColor 
+        <$> (Color <$> v .: "r" <*> v .: "g" <*> v .: "b" <*> v .: "a")
+
+makeWorldAndComponents "World" [''Position, ''Velocity, ''Size, ''Mass, ''EntityID, ''Immovable, ''CircleColor]
+
+data EntityData = EntityData { entityPos   :: Position
+                             , entityVel   :: Velocity
+                             , entityMass  :: Mass
+                             , entitySize  :: Size
+                             , immovable   :: Bool
+                             , entityColor :: CircleColor
+                             }
+
+instance FromJSON EntityData where 
+    parseJSON = withObject "entity" $ \v -> EntityData 
+        <$> v .: "position"
+        <*> v .: "velocity"
+        <*> (Mass <$> v .: "mass")
+        <*> (Size <$> v .: "size")
+        <*> v .:? "immovable" .!= False
+        <*> v .: "color"
+
+data Simulation = Simulation { entities     :: [EntityData] 
+                             , windowWidth  :: Int 
+                             , windowHeight :: Int 
+                             , windowTitle  :: String
+                             , framerate    :: Int
+                             } deriving Generic
+
+instance FromJSON Simulation
+
+gravityConstant :: Float
+gravityConstant = 1
+
+main :: IO ()
+main = do 
+    [filename] <- getArgs
+    Just sim   <- decodeFileStrict @Simulation filename
+
+    win   <- initWindow (windowWidth sim) (windowHeight sim) (windowTitle sim)
+    world <- initWorld
+
+    setTargetFPS (framerate sim)
+    runSystem (mapM_ spawnCircle (entities sim)) world -- spawn initial entities
+
+    whileWindowOpen0 (runSystem gameFrame world)
+    closeWindow win
+
+gameFrame :: System World ()
+gameFrame = updateEntities >> liftIO beginDrawing >> renderWorld >> liftIO endDrawing
+
+updateEntities :: System World ()
+updateEntities = do 
+    delta <- liftIO getFrameTime
+
+    cmapM_ $ \(Position p1, Velocity v, Mass m1, EntityID entity1, _ :: Not Immovable) -> do 
+        vecs <- collect $ \(Position p2, Mass m2, EntityID entity2) -> do 
+            if entity1 /= entity2 then do 
+                let dist  = vectorDistance p1 p2
+                    force = (m1 * m2) / (dist * dist)
+                    acc   = force / m1
+                    vec   = p2 |-| p1
+                    grav  = vectorNormalize vec |* acc |* delta
+                Just grav
+            else Nothing
+
+        let vel = foldl' (|+|) (Vector2 0 0) vecs
+        set entity1 (Velocity (vel |+| v))
+
+    -- Apply velocity to positions  
+    -- Negate velocity if touching another entity (collision detection)
+    cmapM_ $ \(Position currentPos, Velocity v, Size s1, EntityID entity1, _ :: Not Immovable) -> do 
+        let nextPos = currentPos |+| v
+
+        rest <- collect $ \(Position p2, Size s2, EntityID entity2) -> do 
+            if entity1 == entity2 then Nothing else Just (p2, s2)
+
+        if any (circleCollision (nextPos, s1)) rest then do
+            set entity1 (Velocity (additiveInverse v), Position (currentPos |-| v))
+        else do 
+            set entity1 (Position nextPos)
+
+circleCollision :: (Vector2, Float) -> (Vector2, Float) -> Bool
+circleCollision (p1, r1) (p2, r2) = vectorDistance p1 p2 < r1 + r2
+
+renderWorld :: System World ()
+renderWorld = liftIO (clearBackground black) >> renderEntities
+
+renderEntities :: System World ()
+renderEntities = cmapM_ $ \(Position (Vector2 x y), Size size, Mass m, CircleColor color) -> liftIO $ do
+        drawCircle (round x) (round y) size color
+        drawText (show m) (round x) (round y + 20) 10 white
+
+spawnCircle :: EntityData -> System World ()
+spawnCircle entity = do 
+    e <- newEntity (entityPos entity, entityVel entity, entitySize entity, entityMass entity, entityColor entity)
+    when (immovable entity) (set e Immovable)
+    set e (EntityID e)
+
