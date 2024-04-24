6bacf19f64b873931724cd44270d3e5318d140a8
Archaversine <adam.brohl.w@gmail.com>
2024-04-24 13:40:25 -0500

    Make separate module for Camera

--- app/Camera.hs
+++ app/Camera.hs
@@ -1,1 +1,43 @@
 module Camera where
+
+import AppTypes
+
+import Control.Monad.Reader
+
+import Data.Bool
+import Data.IORef
+
+import Raylib.Core
+import Raylib.Types
+
+updateCamera :: App () 
+updateCamera = do 
+    xOff <- asks xOffset
+    yOff <- asks yOffset
+    s    <- asks offsetSpeed
+
+    speed <- liftIO (readIORef s)
+
+    left  <- liftIO $ (||) <$> isKeyDown KeyA <*> isKeyDown KeyLeft
+    right <- liftIO $ (||) <$> isKeyDown KeyD <*> isKeyDown KeyRight 
+    up    <- liftIO $ (||) <$> isKeyDown KeyW <*> isKeyDown KeyUp 
+    down  <- liftIO $ (||) <$> isKeyDown KeyS <*> isKeyDown KeyDown
+
+    let dx = bool 0 1 right - bool 0 1 left
+        dy = bool 0 1 down  - bool 0 1 up
+
+    liftIO $ modifyIORef xOff (subtract (dx * speed))
+    liftIO $ modifyIORef yOff (subtract (dy * speed))
+
+    reset <- liftIO (isKeyPressed KeyR)
+
+    when reset $ liftIO $ do 
+        writeIORef xOff 0
+        writeIORef yOff 0
+
+    rightMouse <- liftIO (isMouseButtonDown MouseButtonRight)
+
+    when rightMouse $ liftIO $ do 
+        Vector2 mouseDX mouseDY <- getMouseDelta
+        modifyIORef xOff (+mouseDX)
+        modifyIORef yOff (+mouseDY)
--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,11 @@ module Main where
 import Apecs
 import AppTypes
 
+import Camera
+
 import Control.Monad.Reader
 
 import Data.Aeson
-import Data.Bool (bool)
 import Data.List (foldl', find)
 import Data.IORef
 
@@ -62,38 +63,6 @@ gameFrame = updateWorld >> liftIO beginDrawing >> renderWorld >> liftIO endDrawi
 
 updateWorld :: App () 
 updateWorld = updateCamera >> updateEntities
-
-updateCamera :: App () 
-updateCamera = do 
-    xOff <- asks xOffset
-    yOff <- asks yOffset
-    s    <- asks offsetSpeed
-
-    speed <- liftIO (readIORef s)
-
-    left  <- liftIO $ (||) <$> isKeyDown KeyA <*> isKeyDown KeyLeft
-    right <- liftIO $ (||) <$> isKeyDown KeyD <*> isKeyDown KeyRight 
-    up    <- liftIO $ (||) <$> isKeyDown KeyW <*> isKeyDown KeyUp 
-    down  <- liftIO $ (||) <$> isKeyDown KeyS <*> isKeyDown KeyDown
-
-    let dx = bool 0 1 right - bool 0 1 left
-        dy = bool 0 1 down  - bool 0 1 up
-
-    liftIO $ modifyIORef xOff (subtract (dx * speed))
-    liftIO $ modifyIORef yOff (subtract (dy * speed))
-
-    reset <- liftIO (isKeyPressed KeyR)
-
-    when reset $ liftIO $ do 
-        writeIORef xOff 0
-        writeIORef yOff 0
-
-    rightMouse <- liftIO (isMouseButtonDown MouseButtonRight)
-
-    when rightMouse $ liftIO $ do 
-        Vector2 mouseDX mouseDY <- getMouseDelta
-        modifyIORef xOff (+mouseDX)
-        modifyIORef yOff (+mouseDY)
 
 updateEntities :: App ()
 updateEntities = do 
